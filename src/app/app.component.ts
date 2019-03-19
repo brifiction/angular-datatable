f7a755887a21539058e803a8bccc9dce85398cd8
brian ng <brianngchongeu@gmail.com>
2019-03-19 13:19:17 +1100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'angular-datatable';
+// }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <div>
+      <ngx-datatable
+        [rows]="rows"
+        [columns]="columns">
+      </ngx-datatable>
+    </div>
+  `
+})
+export class AppComponent {
+  rows = [
+    { name: 'John Smith', gender: 'Male', company: 'Deloitte' },
+    { name: 'Jane Smith', gender: 'Female', company: 'PWC' },
+    { name: 'Molly', gender: 'Female', company: 'Ernst & Young' },
+  ];
+  columns = [
+    { prop: 'name' },
+    { name: 'Gender' },
+    { name: 'Company' }
+  ];
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [NgxDatatableModule, BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
