979d7285f3a7b1380ee8a9d416696507dc32dc05
brian ng <brianngchongeu@gmail.com>
2019-03-25 10:29:15 +1100

    added ngx-datatable module & components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `
-    <div>
-      <ngx-datatable
-        [rows]="rows"
-        [columns]="columns">
-      </ngx-datatable>
-    </div>
-  `
+  templateUrl: `./app.component.html`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  
+
   rows = [
     { name: 'John Smith', gender: 'Male', company: 'Deloitte' },
     { name: 'Jane Smith', gender: 'Female', company: 'PWC' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-
-// import { AppComponent } from './app.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
